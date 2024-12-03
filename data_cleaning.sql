61dd756c913c2de732183534b7b8a01990f76b4d
Adriana <a.m.zejmo@gmail.com>
2024-12-03 12:35:33 +0100

    removed duplicates from dataset

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -5,4 +5,67 @@ FROM layoffs;
 
 -- 1. Remove duplicates if there are any
 -- 2. Standarize the data
--- 3. 
+-- 3. Null Values or blank values
+-- 4. Remove any columns
+
+-- Removing duplicates
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+INSERT INTO layoffs_staging
+SELECT *
+FROM layoffs;
+
+SELECT * 
+FROM layoffs_staging;
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT * 
+from duplicate_cte
+WHERE row_num > 1;
+
+SELECT * 
+FROM layoffs_staging 
+WHERE company = 'Casper';
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SELECT * 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, funds_raised_millions) AS row_num
+FROM layoffs_staging;
+
+DELETE
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2;
